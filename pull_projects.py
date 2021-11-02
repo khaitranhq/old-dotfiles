da890e3516165770f996128f76bd22c49d3d908e
lioaslan <leoalan5577@gmail.com>
2021-11-02 14:41:39 +0700

    update

--- pull_projects.py
+++ pull_projects.py
@@ -13,6 +13,24 @@ LIST_PROJECTS = [{
 }, {
     'dir': 'ticket',
     'git': 'git@github.com:lioaslan/ticket.git'
+}, {
+    'dir': 'ultorex/ultorex-be/common',
+    'git': 'git@gitlab.com:ultorex/backend-group/common.git'
+}, {
+    'dir': 'ultorex/ultorex-be/account',
+    'git': 'git@gitlab.com:ultorex/backend-group/account.git'
+}, {
+    'dir': 'ultorex/ultorex-be/bb',
+    'git': 'git@gitlab.com:ultorex/backend-group/bb.git'
+}, {
+    'dir': 'pbl6',
+    'git': 'git@github.com:Linhnef/PBL6.git'
+}, {
+    'dir': 'ultorex/ultorex-infra/cicd-ansible',
+    'git': 'git@gitlab.com:ultorex/infra-group/cicd-ansible.git'
+}, {
+    'dir': 'ultorex/ultorex-be/hibtc-web',
+    'git': 'git@gitlab.com:ultorex/backend-group/hibtc-web.git'
 }]
 
 for project in LIST_PROJECTS:
