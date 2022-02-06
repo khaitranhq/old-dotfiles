63daa40a64da2df78f20cf20d06b0f768f1d3a12
lioaslan <leoalan5577@gmail.com>
2022-02-06 23:06:25 +0700

    Update rofi

--- .config/rofi/launchers/launcher.sh
+++ .config/rofi/launchers/launcher.sh
@@ -12,11 +12,11 @@
 # gnome_do	slingshot		appdrawer			appdrawer_alt	appfolder
 # column	row				row_center			screen			row_dock		row_dropdown
 
-theme="launchpad"
-dir="$HOME/.config/rofi/launchers/misc"
+theme="blurry_full"
+dir="$HOME/.config/rofi/launchers"
 
 # comment these lines to disable random style
 # themes=($(ls -p --hide="launcher.sh" $dir))
 # theme="${themes[$(( $RANDOM % 16 ))]}"
 
-rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme" -icon-theme McMojave-circle-black
+rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
